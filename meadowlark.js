17cf007fc642c5626f41c0fe3e183206c8a7494c
Peter Yefi <peteryefi@gmail.com>
2018-03-21 11:33:22 +0200

    Added modules for single page testing and also added some single page testing functionalities

--- meadowlark.js
+++ meadowlark.js
@@ -14,6 +14,12 @@ app.set('port', process.env.PORT || 3000);
 //set directory for static files
 app.use(express.static(__dirname + '/public'));
 
+//defining route parameters for testing
+app.use(function(req, res, next){
+	res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+	next();
+});
+
 app.get('/', function(req, res){
 	res.render('home');
 });
@@ -23,13 +29,16 @@ var fortune = require('./lib/fortune.js');
 
 app.get('/about', function(res, res){
 	
-	res.render('about', {fortune: fortune.getFortune()});
+	res.render('about', {
+		fortune: fortune.getFortune(),
+		pageTestScript: '/qa/tests-about.js'
+	});
 });
 
 //custom 404 page
 app.use(function(req, res){
 	res.status(404);
-	res.render('404 - Not Found');
+	res.render('404');
 });
 
 //custom 500 page
@@ -37,7 +46,7 @@ app.use(function(err, req, res, next){
 	console.error(err.stack);
 	
 	res.status(500);
-	res.render('500 - Server Error');
+	res.render('500');
 });
 
 
