bba3bde29de561713e2b5ed7a272d51202c41ae4
Peter Yefi <peteryefi@gmail.com>
2018-03-26 16:09:16 +0200

    Added cookies and sessions

--- meadowlark.js
+++ meadowlark.js
@@ -7,6 +7,13 @@ var bodyParser = require('body-parser');
 var formidable = require('formidable');
 
 var jqupload = require('jquery-file-upload-middleware');
+
+var credentials = require('./credentials.js');
+
+app.use(require('cookie-parser')(credentials.cookieSecret));
+
+app.use(require('express-session')());
+
 //set up handlebars view engine
 var handlebars = require('express3-handlebars').create({
 		defaultLayout:'main',
@@ -39,6 +46,11 @@ app.use(bodyParser.json());
 app.use(function(req, res, next){
 	res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
 	next();
+});
+
+app.use(function(req, res, next){
+	res.locals.flash = req.session.flash;
+	delete req.session.flash;
 });
 
 
@@ -127,19 +139,50 @@ app.get('/data/nursery-rhyme', function(req, res){
 });
 
 app.get('/newsletter', function(req, res){
+	
 	res.render('newsletters', {csrf: 'CSRF token goes here'});
 });
 
 app.post('/process', function(req, res){
-	console.log('Form (from querystring): ' + req.query.form);
- 	console.log('CSRF token (from hidden form field): ' + req.body._csrf);
- 	console.log('Name (from visible form field): ' + req.body.name);
- 	console.log('Email (from visible form field): ' + req.body.email);
- 	if(req.xhr || req.accepts('json,html') === 'json'){
- 		res.send({success:true});
- 	}else{
- 		res.redirect(303, '/thank-you');
- 	}
+	var name = req.body.name || '',
+		email = req.body.email || '';
+
+		if(!email.match(VALID_EMAIL_REGEX)){
+			if(req.xhr)
+				return res.json({error: 'Invalid email address'});
+
+			req.session.flash = {
+				type: 'danger',
+				intro: 'Validation error!',
+				message: 'The email address you entered was not valid',
+			}
+
+			return res.redirect(303, '/newsletter/archive')
+		}
+
+		new NewsletterSignup({name: name, email:email}).save(function(err){
+			if(err){
+				if(req.xhr) 
+					return res.json({error: 'Database error'});
+
+				req.session.flash = {
+					type: 'danger',
+					intro: 'Database error!',
+					message: 'There was a database error. Please try again later'
+				}
+				res.redirect(303, '/newsletters/archive')
+			}
+
+			if(req.xhr)
+				return res.json({success:true});
+
+			req.session.flash = {
+				type: 'success',
+				intro: 'Thank you!',
+				message: 'Thank you now you have been signed up for the newsletter'
+			};
+			res.redirect('/newsletters/archive')
+		});
  	
 });
 
@@ -166,6 +209,7 @@ app.post('/contest/vacation-photo/:year/:month', function(req, res){
 });
 
 app.get('/thank-you', function(req, res){
+	res.cookie('test', 'Testing', {signed: true});
 	res.render('thank-you');
 });
 
@@ -173,7 +217,7 @@ app.use('/upload', function(req, res, next){
 	var now = Date.now();
 	jqupload.fileHandler({
 		uploadDir: function(){
-			return __dirname + '/public/uploads' + now;
+			return __dirname + '/public/uploads/' + now;
 		},
 		uploadUrl: function(){
 			return '/uploads/' + now;
